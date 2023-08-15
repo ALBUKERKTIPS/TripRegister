095572ad1ce4ae515c88dcf44465eb1e5e9899e7
Anderson Albuquerque <118145194+ALBUKERKTIPS@users.noreply.github.com>
2023-08-15 18:37:34 +0100

    updating adm login

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,5 +1,7 @@
+from sqlalchemy.exc import IntegrityError
+
 from app import app, database
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models.tables_db import User  # To create instance inside the route Login
 from app.models.forms import LoginForm, CreateForm  # To create instance inside the route Login
@@ -15,15 +17,20 @@ def start_services():
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:  # If user is authenticated , don't need login
-        return redirect(url_for('start_services'))
+    if current_user.is_authenticated and current_user.position == "ADM":  # If user is authenticated , don't need login
+        redirect(url_for('administration'))
+    else:
+        redirect(url_for('start_services'))
 
     form = LoginForm()
     if form.validate_on_submit():  # Before receive the information, check the fields
         find_user = User.query.filter_by(user=form.username.data).first()  # Asking about username in database
         if find_user and check_password_hash(find_user.password, form.password.data):  # Verify Data User and Pass is in User Data
             login_user(find_user)  # Start Session
-            return redirect(url_for('start_services'))
+            if find_user.position == 'ADM':
+                return redirect(url_for('administration'))
+            else:
+                return redirect(url_for('start_services'))
         else:
             flash('Invalid Login', 'error')
             return redirect(url_for('login'))
@@ -37,16 +44,32 @@ def logout():
 
 
 @app.route('/services')
+@login_required  # Verify if user is Login
 def service():
     render_template("services.html")
 
 
 @app.route('/create-account', methods=['GET', 'POST'])
+@login_required  # Verify if user is Login
 def create_account():
     create_form = CreateForm()
     if create_form.validate_on_submit():
-        with database.session.begin():
-            try:
+        try:  # Verify and save if exist the same, user, email,contact or nif before save in DB
+            duplicate_user = User.query.filter_by(user=create_form.user.data).first()
+            duplicate_email = User.query.filter_by(email=create_form.email.data).first()
+            duplicate_contact = User.query.filter_by(contact=create_form.contact.data).first()
+            duplicate_nif = User.query.filter_by(nif=create_form.nif.data).first()
+
+            if duplicate_user:
+                flash('User que você escolheu já está cadastrado.', 'error')
+            if duplicate_email:
+                flash('Email que você escolheu já está cadastrado.', 'error')
+            if duplicate_contact:
+                flash('Contact que você escolheu já está cadastrado.', 'error')
+            if duplicate_nif:
+                flash('NIF que você escolheu já está cadastrado.', 'error')
+
+            if not(duplicate_user or duplicate_contact or duplicate_email or duplicate_nif):
                 new_user = User(nif=create_form.nif.data,
                                 name=create_form.name.data,
                                 position=create_form.position.data,
@@ -56,11 +79,29 @@ def create_account():
                                 )
                 new_user.set_password(create_form.password.data)  # Making Hash
                 database.session.add(new_user)
-                print('salvou no banco!')
+                database.session.commit()
+                print('SAVE IN DB!')
                 flash('Account Created Successfully', 'success')
-                return redirect(url_for('login'))  # After create, go to login page
-            except Exception as e:
-                database.session.rollback()  # revert the process in case error ( hash or something)
-                print('deu erro!')
-                flash('Failed to create account. Please try again', 'error')
+        except IntegrityError as e:
+            database.session.rollback()  # revert the process in case error ( hash or something)
+            print('DON T SAVE IN DB')
+            flash('Failed to create account. Please try again', 'error')
     return render_template('register.html', create_form=create_form)
+
+
+@app.route('/see-all-users')
+@login_required  # Verify if user is Login
+def see_all_users():
+    users = User.query.all()
+    return render_template('all_users.html', users=users)
+
+
+@app.route('/adm')
+@login_required  # Verify if user is Login
+def administration():
+    return render_template('adm.html', user=current_user)
+
+
+@app.errorhandler(401)  # Capture the 401 error
+def unauthorized(error):
+    return render_template('not_authorized.html', message="Voçê não tem permissão. Obrigatório fazer login"), 401
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, EmailField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+import re
 
 
 class LoginForm(FlaskForm):
@@ -11,10 +12,15 @@ class LoginForm(FlaskForm):
 class CreateForm(FlaskForm):
     nif = IntegerField("NIF", validators=[DataRequired()])
     name = StringField("Full Name", validators=[DataRequired()])
-    position = StringField("Position", validators=[DataRequired()])
+    position_choices = [("ADM", "Administration"),
+                        ("TRANSFER", "Transfer"),
+                        ("WASHER", "Washer"),
+                        ("ALL", "Do Everything")]
+    position = SelectField("Position", choices=position_choices, validators=[DataRequired()])
     contact = IntegerField("Phone Number", validators=[DataRequired()])
     email = EmailField("Email", validators=[DataRequired(), Email()])
     user = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm your password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm your password', validators=[DataRequired(), EqualTo('password', message='As senhas não são iguais')])
+    print('submeteu a senha')
     submit = SubmitField('Create')
